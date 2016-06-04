56a2e96c3d7982c124b062b4987fbddcbdab1480
wataken44 <wataken44@gmail.com>
2016-06-04 12:42:49 +0900

    [A] ex5, ex6, ex7, ex8

---
+++ machine-learning-ex5/ex5/testLearningCurve.m
@@ -0,0 +1,7 @@
+function f = testLearningCurve()
+  X = [1 2 3 4];
+  y = 5;
+  theta = [0.1 0.2 0.3 0.4]';
+  [J g] = linearRegCostFunction(X, y, theta, 7)
+  f = 0
+end
---
+++ machine-learning-ex5/ex5/testLinearRegCostFunction.m
@@ -0,0 +1,7 @@
+function g = testLinearRegCostFunction()
+  X = [[1 1 1]' magic(3)];
+  y = [7 6 5]';
+  theta = [0.1 0.2 0.3 0.4]';
+  [J g] = linearRegCostFunction(X, y, theta, 0)
+  [J g] = linearRegCostFunction(X, y, theta, 7)
+end
